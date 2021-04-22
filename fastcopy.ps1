9437678acf8f6164c3982cfef0fad22233d4f71d
Kirubel K <36381793+joejnke@users.noreply.github.com>
2021-04-22 14:02:37 +0300

    Restructured code for cmdlet

--- fastcopy.ps1
+++ fastcopy.ps1
@@ -22,84 +22,97 @@
 #
 # This script will throttle the number of concurrent jobs based on $max_jobs
 #
-$max_jobs = 8
-$tstart = get-date
 
-#
-# Set $src to a directory with lots of sub-directories
-#
-$src = $args[0] + "\"
-Write-Host "Source dir: " $src
+Function fscp {
+	[CmdletBinding()]
 
-#
-# Set $dest to a local folder or share you want to back up the data to
-#
-$dest = $args[1] + "\"
-Write-Host "Destination dir: " $dest
+	Param(
+		[Parameter (Mandatory=$true, HelpMessage="Path to source directory to copy from")]
+		[String]$srcIn,
 
-#
-# Set $log to a local folder to store logfiles
-#
-$log = $dest + "\fastcopy_log"
-mkdir $log
-$log += "\$(get-date -f yyyy-MM-dd-mm-ss).log"
+		[Parameter (Mandatory=$true, HelpMessage="Path to destination directory to copy to")]
+		[String]$destIn
+	)
 
-#the function that use robocopy 
-$FastCopy = {
-	param($name, $src, $dest, $log)
-	if ($name -eq "FL"){
-		ls $name | %{Start-Job -Name $_ $FastCopy -ArgumentList $_,$src,$dest,$log | Out-null}
-	}
-	else {
-		robocopy $src$name $dest$name /E /nfl /np /mt:16 /ndl /LOG+:$log | Out-null  #the copy command
-	}
-}
+	Begin {
+		#source directory
+		$src = $srcIn + "\"
 
-#list directories in $src
-$files = ls $src
+		#destination directory
+		$dest = $destIn + "\"
 
-#for each directory name in $files.
-$files | %{
-	#check the number of jobs running and Start-Sleep untill the number gets below the max_jobs
-	$j = Get-Job -State "Running"
-	while ($j.count -ge $max_jobs) 
-	{
-		 Start-Sleep -Milliseconds 500
-		 $j = Get-Job -State "Running"
+		#logging directory and log file
+		$log = $dest + "\fastcopy_log"
+		mkdir $log
+		$log += "\$(get-date -f yyyy-MM-dd-mm-ss).log"
 	}
 
-	Get-job -State "Completed" | Receive-job	#delete job result of those completed
-	Remove-job -State "Completed"	#terminate or delete completed jobs
-	Start-Job -Name $_ $FastCopy -ArgumentList $_,$src,$dest,$log | Out-null 
-    Write-Host -NoNewline "." #to show progress
-}
+	Process {
+		#the function that uses robocopy 
+		$FastCopy = {
+			param($name, $src, $dest, $log)
+			robocopy $src$name $dest$name /E /nfl /np /mt:16 /ndl /LOG+:$log | Out-null
+		}
 
-#
-# No more jobs to process. Wait for all of them to complete
-#
+		#print source and destination directories
+		Write-Host "Source dir: " $src
+		Write-Host "Destination dir: " $dest
 
-Write-Host "`nwaiting for all jobs to complete ..."
-Write-host "# of Remaining jobs: "
+		$max_jobs = 8
+		$tstart = get-date
 
-$rjobs = Get-Job -State "Running" 
-$jobcount = $rjobs.count
-While (Get-Job -State "Running") {
-	$rjobs = Get-Job -State "Running"
-	if ($rjobs.count -lt $jobcount) {
+		#list directories in source dir
+		$files = ls $src
+
+		#for each directory in source dir apply threaded robocopy command.
+		$files | %{
+			#check the number of jobs running and Start-Sleep untill the number gets below the max_jobs
+			$j = Get-Job -State "Running"
+			while ($j.count -ge $max_jobs) 
+			{
+				 Start-Sleep -Milliseconds 500
+				 $j = Get-Job -State "Running"
+			}
+
+			Get-job -State "Completed" | Receive-job	#delete job result of those completed
+			Remove-job -State "Completed"	#terminate or delete completed jobs
+
+			if ($name -eq "FL"){
+				ls $name | %{Start-Job -Name $_ $FastCopy -ArgumentList $_,$src,$dest,$log | Out-null}
+			}
+			else {
+				Start-Job -Name $_ $FastCopy -ArgumentList $_,$src,$dest,$log | Out-null
+			}
+		    Write-Host -NoNewline "." #to show progress
+		}
+
+	}
+
+	End {
+		Write-Host "`nwaiting for all jobs to complete ..."
+		Write-host "# of Remaining jobs: "
+
+		$rjobs = Get-Job -State "Running" 
 		$jobcount = $rjobs.count
-		Write-host $rjobs.Name.count
+		While (Get-Job -State "Running") {
+			$rjobs = Get-Job -State "Running"
+			if ($rjobs.count -lt $jobcount) {
+				$jobcount = $rjobs.count
+				Write-host $rjobs.Name.count
+			}
+			Start-Sleep 2 
+		}
+
+		Remove-Job -State "Completed" 
+		Write-Host "`ncopy completed ..."
+
+		$tend = get-date
+
+		$tspan = new-timespan -start $tstart -end $tend
+		$tspanHour = ($tspan.TotalSeconds/3600 - ($tspan.TotalSeconds%3600)/3600)
+		$tspanMin = ($tspan.TotalSeconds%3600/60 - ( $tspan.TotalSeconds%3600%60)/60)
+		$tspanSec = ($tspan.TotalSeconds%3600%60)
+
+		Write-host "`nTotal time to copy " $tspanHour":"$tspanMin":"$tspanSec
 	}
-	Start-Sleep 2 
 }
-
-Remove-Job -State "Completed" 
-Write-Host "`ncopy completed ..."
-
-$tend = get-date
-
-$tspan = new-timespan -start $tstart -end $tend
-$tspanHour = ($tspan.TotalSeconds/3600 - ($tspan.TotalSeconds%3600)/3600)
-$tspanMin = ($tspan.TotalSeconds%3600/60 - ( $tspan.TotalSeconds%3600%60)/60)
-$tspanSec = ($tspan.TotalSeconds%3600%60)
-
-Write-host "`nTotal time to copy " $tspanHour":"$tspanMin":"$tspanSec
